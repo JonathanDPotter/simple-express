c410d6ccd1eb5950b65218f6a9ebd7aa11441bbe
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-11-28 14:23:36 -0500

    refactored simpler

--- index.js
+++ index.js
@@ -2,25 +2,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const router = express.Router();
+const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "public");
 
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-  res.sendFile(index.html);
-});
-
-app.get("/contact", (req, res) => {
-  res.sendFile(publicPath + "/contact.html");
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(publicPath + "/about.html");
-});
-
-app.get("/*", (req, res) => {
-  res.sendFile(publicPath + "/404.html");
-});
-
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => console.log(`Server runnning on port: ${PORT}`));
